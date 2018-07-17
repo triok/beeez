ad1527cdc8a441ac18e12ed641da757dded7f545
Denis Sinyukov <sinyukov.denis2015@yandex.ru>
2018-07-17 23:35:27 +0300

    change mail text

--- app/Mail/NewCommentMail.php
+++ app/Mail/NewCommentMail.php
@@ -8,23 +8,19 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewCommentNotice extends Mailable
+class NewCommentMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    /** @var Comment $comment */
     public $comment;
+    /** @var Job $job */
     public $job;
 
-    /**
-     * Create a new message instance.
-     *
-     * @param Comment $comment
-     * @param Job $job
-     */
     public function __construct(Comment $comment, Job $job)
     {
         $this->comment = $comment;
-        $this->job = $job;
+        $this->job     = $job;
     }
 
     /**
@@ -34,10 +30,8 @@ class NewCommentNotice extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.new-comment')
+        return $this->markdown('emails.comments.new')
             ->subject('There is a new comment for your task')
-            ->from(env('MAIL_FROM_ADDRESS', env('MAIL_FROM_NAME')));
+            ->from(auth()->user()->email, auth()->user()->name);
     }
-
-
 }
