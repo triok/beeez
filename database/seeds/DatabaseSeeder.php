ea8e749c8583ce8c8a2e3132255fecf332c6c728
Oleg Prokopiv <oleg.prokopiv@gmai.com>
2018-09-03 13:15:16 +0300

    add torann/currency

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
          $this->call(SocialTableSeeder::class);
          $this->call(TeamTypesTableSeeder::class);
          $this->call(PagesTableSeeder::class);
-       
+         $this->call(CurrenciesTableSeeder::class);
+
     }
 }
