dff6f2ce608887462c5c99643647a65a0b7d47f6
Oleg Prokopiv <oleg.prokopiv@gmai.com>
2018-10-08 16:36:43 +0300

    added users sorting

--- resources/views/peoples/peoples-index.blade.php
+++ resources/views/peoples/peoples-index.blade.php
@@ -1,37 +1,84 @@
 @extends('layouts.app')
+
 @section('content')
-<div class="container-fluid" id="main">
-   <div class="row">
-      <div class="col-md-12">
-         <h2>@lang('peoples.title')</h2>
-         <div class="row">
-            <div class="col-md-3 search">
-               <input type="text" class="form-control" id="login_search" placeholder="@lang('peoples.search')"><i class="fa fa-search" aria-hidden="true"></i>
-               <ul class="result"></ul>
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>@lang('peoples.title')</h2>
+                <div class="row">
+                    <div class="col-md-3 search">
+                        <input type="text" class="form-control" id="login_search" placeholder="@lang('peoples.search')"><i
+                                class="fa fa-search" aria-hidden="true"></i>
+                        <ul class="result"></ul>
+                    </div>
+                </div>
+                <table class="table table-responsive table-full-width table-peoples table-hover table-search"
+                       id="users-table">
+                    <thead>
+                    <tr>
+                        <th>@lang('peoples.login')</th>
+                        <th>@lang('peoples.name')</th>
+                        <th>@lang('peoples.feedbacks')</th>
+                        <th>@lang('peoples.member')</th>
+                    </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
             </div>
-         </div>
-         <table class="table table-responsive table-full-width table-peoples table-hover table-search">
-            <thead>
-            <tr>
-               <th>@lang('peoples.login')</th>
-               <th>@lang('peoples.name')</th>
-               <th>@lang('peoples.feedbacks')</th>               
-               <th>@lang('peoples.member')</th>               
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($users as $user)
-               <tr>
-                  <td><a href="{{route('peoples.show', $user)}}">{{$user->username}}</a></td>
-                  <td>{{$user->name}}</td>
-                  <td><span class="text-success">{{$user->rating_positive}}</span>/<span class="text-danger">{{$user->rating_negative}}</span></td>
-                  <td class="date-short">{{ $user->created_at }}</td>
-               </tr>
-            @endforeach
-            </tbody>
-         </table>
-         {!! $users->links() !!}
-      </div>
-   </div>
-</div>
+        </div>
+    </div>
 @endsection
+
+@push('styles')
+    <link href="/css/custom.css" rel="stylesheet">
+@endpush
+
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            $('#users-table').DataTable({
+                bFilter: false,
+                bInfo: false,
+                "lengthChange": false,
+
+                "ajax": {
+                    "url": "api/v1/users",
+                    "dataSrc": ""
+                },
+                "columns": [
+                    {
+                        "data": "username",
+                        "render": function (data, type, row, meta) {
+                            if (type === 'display') {
+                                return '<a href="peoples/' + row.id + '">' + data + '</a>';
+                            }
+
+                            return data;
+                        }
+                    },
+                    {"data": "name"},
+                    {
+                        "data": "rating_positive",
+                        "render": function (data, type, row, meta) {
+                            if (type === 'display') {
+                                return '<span class="text-success">' + data + '</span>/<span class="text-danger">' + row.rating_negative + '</span>';
+                            }
+
+                            return data;
+                        }
+                    },
+                    {
+                        "data": "created_at",
+                        "render": function (data, type, row, meta) {
+                            if (type === 'display') {
+                                return '<span class="date-short">' + data + '</span>';
+                            }
+
+                            return data;
+                        }
+                    },
+                ]
+            });
+        });
+    </script>
+@endpush
