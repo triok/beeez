58e78ed7fda53ac43d3cd262a1f050aafa4587d5
Oleg Prokopiv <oleg.prokopiv@gmai.com>
2019-01-16 23:36:15 +0200

    4 user online

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Locale::class,
             \Torann\Currency\Middleware\CurrencyMiddleware::class,
+            \App\Http\Middleware\UserOnline::class,
         ],
 
         'api' => [
---
+++ resources/views/partials/online.blade.php
@@ -0,0 +1,5 @@
+@if($user->isOnline())
+    <i style="color:green;font-size: 11px;" class="fa fa-circle"></i>
+@else
+    <i style="color:lightgray;font-size: 11px;" class="fa fa-circle"></i>
+@endif
