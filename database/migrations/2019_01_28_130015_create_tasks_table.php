aa9b35585e5cd61f5c8f9fd455438da581f8b135
Oleg Prokopiv <oleg.prokopiv@gmai.com>
2019-01-29 11:58:03 +0200

    Added tasks

---
+++ database/migrations/2019_01_28_130015_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name', 100);
+            $table->date('do_date');
+            $table->boolean('completed')->default(false);
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
