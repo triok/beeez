c887316e57acdbff26517888e1dbd321c0222e1b
Oleg Prokopiv <oleg.prokopiv@gmai.com>
2019-03-22 16:15:53 +0200

    Added cancel deal

---
+++ app/Notifications/JobNotDeclinedNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Jobs\Job;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class JobNotDeclinedNotification extends Notification
+{
+    use Queueable;
+
+    protected $job;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Job $job
+     */
+    public function __construct(Job $job)
+    {
+        $this->job = $job;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'job_id' => $this->job->id,
+        ];
+    }
+}
